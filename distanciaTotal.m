441676cd95336e8b6f6a345429c9307b62d02996
Tuca-DEV <guga_camil@hotmail.com>
2023-02-01 15:02:22 -0400

    reajustes na função populacaoInit e distanciaTotal

--- distanciaTotal.m
+++ distanciaTotal.m
@@ -1,20 +1,20 @@
-function [distanciaTotal] = caixeiroViajante(OrdemCidadesVisitadas, DistCidades)
+function [distancia] = distanciaTotal(OrdemCidadesVisitadas, DistCidades)
 
   for i=1:length(OrdemCidadesVisitadas)
 
-    distanciaTotal(i) = 0;
+    distancia(i) = 0;
     pos1 = OrdemCidadesVisitadas(i,1);
 
     for j=1:length(OrdemCidadesVisitadas)-1
 
       pos2 = OrdemCidadesVisitadas(i,j+1);
-      distanciaTotal(i) += DistCidades(pos1, pos2);
+      distancia(i) += DistCidades(pos1, pos2);
       pos1 = pos2;
 
     endfor
 
   endfor
 
-  disp(distanciaTotal');
+  disp(distancia);
 
 endfunction
