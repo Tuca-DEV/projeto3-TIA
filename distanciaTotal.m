264add895e0fcf7015ddb9ff56fcc29e2c93dd85
Tuca-DEV <guga_camil@hotmail.com>
2023-02-01 15:22:03 -0400

    Função de cálculo da distância total de um cromossomo concluída

--- distanciaTotal.m
+++ distanciaTotal.m
@@ -1,17 +1,12 @@
-function [distancia] = distanciaTotal(OrdemCidadesVisitadas, DistCidades)
+function [distancia] = distanciaTotal(OrdemCidadesVisitadas, tabela_distancias)
 
-  for i=1:length(OrdemCidadesVisitadas)
+  distancia = 0;
 
-    distancia(i) = 0;
-    pos1 = OrdemCidadesVisitadas(i,1);
+  for i = 1:(length(OrdemCidadesVisitadas)-1);
 
-    for j=1:length(OrdemCidadesVisitadas)-1
-
-      pos2 = OrdemCidadesVisitadas(i,j+1);
-      distancia(i) += DistCidades(pos1, pos2);
-      pos1 = pos2;
-
-    endfor
+    cidade1 = OrdemCidadesVisitadas(i);
+    cidade2 = OrdemCidadesVisitadas(i+1);
+    distancia += tabela_distancias(cidade1, cidade2);
 
   endfor
 
