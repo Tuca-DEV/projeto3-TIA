48137d3e76d075292cb34798d355931152e730d7
Livia Caetano de Andrade <62720700+Livia-CA@users.noreply.github.com>
2023-02-08 15:59:40 -0300

    Update main.m

--- main.m
+++ main.m
@@ -1,5 +1,21 @@
-caminho = init_caminho();
-populacao = populacaoInit();
 distanciaCidades = distanciaTotal(populacao, caminho);
 pais = elitismo(distanciaCidades);
 filhos = crossOver(pais, populacao)
+
+
+%%%%%%%%%%%%%%%%%%%%%%
+caminho = init_caminho();
+populacao = populacaoInit();
+melhor_da_geracao = 0;
+i = 1;
+
+while (melhor_da_geracao > 20)
+  distanciaCidades = distanciaTotal(populacao, caminho);
+   melhor_da_geracao(i) = max(distanciaCidades);
+   
+   i++ 
+endwhile
+
+
+plot((1:length(melhor_da_geracao)), melhor_da_geracao)
+
