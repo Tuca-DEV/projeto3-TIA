9b3fb22cf8516540c44a71ab0b8c3dc0f088f986
Tuca-DEV <guga_camil@hotmail.com>
2023-02-08 16:26:40 -0400

    Main com elitismo

--- main.m
+++ main.m
@@ -1,20 +1,26 @@
-distanciaCidades = distanciaTotal(populacao, caminho);
-pais = elitismo(distanciaCidades);
-filhos = crossOver(pais, populacao)
-
-
-%%%%%%%%%%%%%%%%%%%%%%
 caminho = init_caminho();
 populacao = populacaoInit();
-melhor_da_geracao = 0;
-i = 1;
+i = 0;
+melhor_da_geracao = inf(1,50);
+do
+   populacao
+   %Cálculo das distâncias totais de cada indivíduo
+   i++;
+   distanciaCidades = distanciaTotal(populacao, caminho)
+   melhor_da_geracao(i) = min(distanciaCidades)
 
-while (melhor_da_geracao > 20)
-  distanciaCidades = distanciaTotal(populacao, caminho);
-   melhor_da_geracao(i) = max(distanciaCidades);
-   
-   i++ 
-endwhile
+   %Seleção -> Cruzamento -> Mutação -> Inserção
+   pais = elitismo(distanciaCidades)
+   filhos = crossOver(pais, populacao);
+   filhos = mutacao(filhos)
+   populacao(16:17, :) = filhos;
+
+   %Remoção dos 2 piores caminhos
+   for c = 1:2
+     distanciaCidades = distanciaTotal(populacao, caminho);
+     populacao(pais(1,c), :) = [];
+   endfor
+until(melhor_da_geracao(i) < 11)
 
 
 plot((1:length(melhor_da_geracao)), melhor_da_geracao)
